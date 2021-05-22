835eaac19561efcd950f85d524e7eb5613e5e1ae
MediQ <78607177+Niobiyd@users.noreply.github.com>
2021-05-22 22:39:44 +0300

    Create return.lua

---
+++ return.lua
@@ -0,0 +1,32 @@
+local PLUGIN = PLUGIN
+PLUGIN.Name = 'Return to death position.'
+PLUGIN.Author = 'Chancer [NS] | MediQ [ix]'
+PLUGIN.Schema = 'Any'
+
+function PLUGIN:PlayerDeath( victim, inflictor, attacker )
+	local char = victim:GetCharacter()
+	
+	if ( char ) then
+		char:SetData( 'deathPos', victim:GetPos())
+	end
+end
+
+ix.command.Add( 'ReturnDeathPos', {
+	OnRun = function( client, arguments )
+		if ( IsValid( client ) ) then
+			if ( client:Alive() ) then
+				local char = client:GetCharacter()
+				local oldPos = char:GetData("deathPos")
+				
+				if ( oldPos ) then
+					client:SetPos( oldPos )
+					char:SetData( 'deathPos', nil )
+				else
+					client:Notify( 'No death position saved.' )
+				end
+			else
+				client:Notify( 'Wait until you respawn.' )
+			end
+		end
+	end
+})
