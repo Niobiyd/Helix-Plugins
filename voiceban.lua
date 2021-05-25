fccb0bb8ba468b2f7f76c612f208df2844531731
MediQ <78607177+Niobiyd@users.noreply.github.com>
2021-05-25 12:20:03 +0300

    Update voiceban.lua

--- voiceban.lua
+++ voiceban.lua
@@ -1,28 +1,34 @@
 
 
--- ██████████████████████████████████████████████████████████████████████████████████████████████████████████████
--- █─▄▄▄─█─▄▄─█▄─▄▄▀█▄─▄▄─███▄─█▀▀▀█─▄█▄─▄▄▀█▄─▄█─▄─▄─█▄─▄▄─█▄─▄▄▀███▄─▄─▀█▄─█─▄███▄─▀█▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄█─▄▄▄─█
--- █─███▀█─██─██─██─██─▄█▀████─█─█─█─███─▄─▄██─████─████─▄█▀██─██─████─▄─▀██▄─▄█████─█▄█─███─▄█▀██─██─██─██─██▀─█
--- ▀▄▄▄▄▄▀▄▄▄▄▀▄▄▄▄▀▀▄▄▄▄▄▀▀▀▀▄▄▄▀▄▄▄▀▀▄▄▀▄▄▀▄▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▀▀▀▀▄▄▄▄▀▀▀▄▄▄▀▀▀▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀▀▄▄▄▀───▄▄▀
+// ██████████████████████████████████████████████████████████████████████████████████████████████████████████████
+// █─▄▄▄─█─▄▄─█▄─▄▄▀█▄─▄▄─███▄─█▀▀▀█─▄█▄─▄▄▀█▄─▄█─▄─▄─█▄─▄▄─█▄─▄▄▀███▄─▄─▀█▄─█─▄███▄─▀█▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄█─▄▄▄─█
+// █─███▀█─██─██─██─██─▄█▀████─█─█─█─███─▄─▄██─████─████─▄█▀██─██─████─▄─▀██▄─▄█████─█▄█─███─▄█▀██─██─██─██─██▀─█
+// ▀▄▄▄▄▄▀▄▄▄▄▀▄▄▄▄▀▀▄▄▄▄▄▀▀▀▀▄▄▄▀▄▄▄▀▀▄▄▀▄▄▀▄▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▀▀▀▀▄▄▄▄▀▀▀▄▄▄▀▀▀▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀▀▄▄▄▀───▄▄▀
 
 
---  Copyright [2021] [MediQ]
+//  Copyright [2021] [MediQ]
 
---  Licensed under the Apache License, Version 2.0 (the 'License');
---  you may not use this file except in compliance with the License.
---  You may obtain a copy of the License at
+//  Licensed under the Apache License, Version 2.0 (the 'License');
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 
---     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 
---  Unless required by applicable law or agreed to in writing, software
---  distributed under the License is distributed on an 'AS IS' BASIS,
---  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
---  See the License for the specific language governing permissions and
---  limitations under the License.
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an 'AS IS' BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 
---	Steam:   https://steamcommunity.com/profiles/76561198843899799/
---	github:  https://github.com/Niobiyd
---	discord: .Медий#9657
+// Code writen by MEDIQ 
+// File created: 20.05.2021
+// Last Edit: 25.05.2021
+// Enjoy!
+// My contacts:
+
+// Discord     :       .Медий#9657
+// Github      :       https://github.com/Niobiyd
+// Steam       :       https://steamcommunity.com/profiles/76561198843899799/
 
 
 local PLUGIN = PLUGIN
